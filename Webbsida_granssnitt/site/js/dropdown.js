5578111dc6ba2c615aee6ca1519c5169da86d732
itggot-david-petersson <david.petersson@itggot.se>
2018-03-08 22:09:45 +0100

    två sista sidorna

--- Webbsida_granssnitt/site/js/dropdown.js
+++ Webbsida_granssnitt/site/js/dropdown.js
@@ -15,3 +15,21 @@ window.onclick = function(event) {
     }
   }
 }  
+
+
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}  
